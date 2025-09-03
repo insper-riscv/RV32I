d7b07f6b68b3243c436371f0b4f6b09eb6c7a403
ilacftemp <ilachafin@gmail.com>
2025-09-03 11:56:10 -0300

    ULA autocontida (alteracao da versao do grupo anterior)

--- src/ULA32bits.vhd
+++ src/ULA32bits.vhd
@@ -2,101 +2,87 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-library WORK;
-use WORK.GENERICS.ALL;
-
-entity ULA32bits is
-
-    generic (
-        DATA_WIDTH : natural := WORK.RV32I.XLEN
-    );
-
-    port (
-        select_function : in  std_logic_vector(3 downto 0);
-        source_1        : in  std_logic_vector((DATA_WIDTH - 1) downto 0);
-        source_2        : in  std_logic_vector((DATA_WIDTH - 1) downto 0);
-        overflow        : out std_logic;
-        destination     : out std_logic_vector((DATA_WIDTH - 1) downto 0)
-    );
-
+entity RV32I_ALU is
+  generic(DATA_WIDTH:natural:=32);
+  port(
+    select_function:in std_logic_vector(3 downto 0);
+    source_1,source_2:in std_logic_vector(DATA_WIDTH-1 downto 0);
+    overflow:out std_logic;
+    destination:out std_logic_vector(DATA_WIDTH-1 downto 0)
+  );
 end entity;
 
-architecture RTL of ULA32bits is
+architecture RTL of RV32I_ALU is
+  signal flag_subtract:std_logic;
+  signal s2_eff,pg,pp,sum,logic_and,logic_or,half_add:std_logic_vector(DATA_WIDTH-1 downto 0);
+  signal carry:std_logic_vector(DATA_WIDTH downto 0);
+  signal add_overflow:std_logic;
+  signal slt,sltu,shift_out,dst_grp0,dst_grp1:std_logic_vector(DATA_WIDTH-1 downto 0);
 
-    signal flag_subtract     : std_logic;
-    signal source_2_auxiliar : std_logic_vector((DATA_WIDTH - 1) downto 0);
-    signal source_and        : std_logic_vector((DATA_WIDTH - 1) downto 0);
-    signal source_or         : std_logic_vector((DATA_WIDTH - 1) downto 0);
-    signal half_add          : std_logic_vector((DATA_WIDTH - 1) downto 0);
-    signal full_add          : std_logic_vector((DATA_WIDTH - 1) downto 0);
-    signal carry_out         : std_logic_vector((DATA_WIDTH - 1) downto 0);
-    signal slt               : std_logic_vector((DATA_WIDTH - 1) downto 0);
-    signal sltu              : std_logic_vector((DATA_WIDTH - 1) downto 0);
-    signal shift             : std_logic_vector((DATA_WIDTH - 1) downto 0);
-    signal destination_1     : std_logic_vector((DATA_WIDTH - 1) downto 0);
-    signal destination_2     : std_logic_vector((DATA_WIDTH - 1) downto 0);
-    signal add_overflow      : std_logic;
-	 
-	 signal carry : std_logic_vector(8 downto 0);
-
+  function sll_vec(a:std_logic_vector;sh:natural)return std_logic_vector is
+    variable r:std_logic_vector(a'range);
+  begin
+    r:=std_logic_vector(shift_left(unsigned(a),sh)); return r;
+  end;
+  function srl_vec(a:std_logic_vector;sh:natural)return std_logic_vector is
+    variable r:std_logic_vector(a'range);
+  begin
+    r:=std_logic_vector(shift_right(unsigned(a),sh)); return r;
+  end;
+  function sra_vec(a:std_logic_vector;sh:natural)return std_logic_vector is
+    variable r:std_logic_vector(a'range);
+  begin
+    r:=std_logic_vector(shift_right(signed(a),sh)); return r;
+  end;
 begin
+  flag_subtract <= (select_function(3 downto 2)="10") and not(select_function(1 downto 0)="01");
+  s2_eff        <= source_2 when flag_subtract='0' else not source_2;
 
-    flag_subtract <=    is_equal_dynamic(select_function(3 downto 2), "10") AND
-                        NOT(is_equal_dynamic(select_function(1 downto 0), "01"));
-                        --WORK.GENERICS.is_equal_dynamic(select_function(3 downto 2), "10") AND
-                        --NOT(WORK.GENERICS.is_equal_dynamic(select_function(1 downto 0), "01"));
+  logic_and <= source_1 and s2_eff;
+  logic_or  <= source_1 or  source_2;
+  half_add  <= source_1 xor s2_eff;
 
-    source_and <= source_1 AND source_2_auxiliar;
-    source_or  <= source_1 OR  source_2;
-    half_add   <= source_1 XOR source_2_auxiliar;
-    full_add   <= half_add XOR (carry_out((DATA_WIDTH - 2) downto 0) & flag_subtract);
+  carry(0) <= flag_subtract;
+  gen_carry:for i in 0 to DATA_WIDTH-1 generate
+    carry(i+1) <= (carry(i) and half_add(i)) or logic_and(i);
+  end generate;
+  sum <= half_add xor carry(DATA_WIDTH-1 downto 0);
 
-    add_overflow <= carry_out(DATA_WIDTH - 1) XOR carry_out(DATA_WIDTH - 2);
-    overflow     <= add_overflow;
+  add_overflow <= carry(DATA_WIDTH) xor carry(DATA_WIDTH-1);
 
-    slt <=  (0 => add_overflow XOR full_add(DATA_WIDTH - 1), others => '0');
-    sltu <= (0 => NOT(carry_out(DATA_WIDTH - 1)),            others => '0');
-		  
-	 source_2_auxiliar <= (source_2 AND NOT(flag_subtract)) OR (NOT(source_2) AND flag_subtract);
-		  
-    -- carry lookahead
-		  
-	 carry(0)  <= flag_subtract;
-    carry_out <= carry(8 downto 1);
+  slt  <= (0 => (add_overflow xor sum(DATA_WIDTH-1)), others => '0');
+  sltu <= (0 => not carry(DATA_WIDTH),                 others => '0');
 
-    BIT_TO_BIT : for i in 0 to (8 - 1) generate
-        carry(i + 1) <= (carry(i) AND half_add(i)) OR source_and(i);
-    end generate;
-	 
-	 --
+  process(all)
+    variable shamt:natural;
+  begin
+    shamt := to_integer(unsigned(source_2(4 downto 0)));
+    if select_function(2)='1' then
+      shift_out <= sll_vec(source_1,shamt);
+    else
+      if select_function(3)='1' then
+        shift_out <= sra_vec(source_1,shamt);
+      else
+        shift_out <= srl_vec(source_1,shamt);
+      end if;
+    end if;
+  end process;
 
-    SHIFTER : entity WORK.RV32I_ALU_SHIFTER
-        generic map (
-            DATA_WIDTH  => WORK.RV32I.XLEN
-        )
-        port map (
-            select_function => select_function,
-            shamt           => source_2(4 downto 0),
-            source          => source_1,
-            destination     => shift
-        );
-		  
-	 destination_1 <=  (
-                        (full_add AND (NOT(select_function(0)) AND NOT(select_function(1)))) OR
-                        (shift AND (select_function(0) AND NOT(select_function(1))))
-                    ) OR (
-                        (slt AND (NOT(select_function(0)) AND select_function(1))) OR
-                        (sltu AND (select_function(0) AND select_function(1)))
-                    );
-		  
-	 destination_2 <=  (
-								(half_add AND (NOT(select_function(0)) AND NOT(select_function(1)))) OR
-								(shift AND (select_function(0) AND NOT(select_function(1))))
-						  ) OR (
-								(source_or AND (NOT(select_function(0)) AND select_function(1))) OR
-								(source_and AND (select_function(0) AND select_function(1)))
-						  );
-		  
-	 destination <= (destination_1 AND NOT(select_function(2))) OR (destination_2 AND select_function(2));
+  with select_function(1 downto 0) select
+    dst_grp0 <=
+      sum       when "00",
+      shift_out when "01",
+      slt       when "10",
+      sltu      when others;
 
+  with select_function(1 downto 0) select
+    dst_grp1 <=
+      (source_1 xor source_2) when "00",
+      shift_out               when "01",
+      logic_or                when "10",
+      (source_1 and source_2) when others;
+
+  destination <= dst_grp0 when select_function(2)='0' else dst_grp1;
+
+  overflow <= add_overflow when (select_function(2)='0' and select_function(1 downto 0)="00") else '0';
 end architecture;
