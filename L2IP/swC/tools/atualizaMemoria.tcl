64e1cbc912eb8be0477335a4eb2747a48003a4c7
Lucas Lima <lucasfsl@al.insper.edu.br>
2025-11-06 15:13:12 -0300

    top level 3 estagios

---
+++ L2IP/swC/tools/atualizaMemoria.tcl
@@ -0,0 +1,20 @@
+# Load Quartus Prime Tcl Package
+package require  ::quartus::insystem_memory_edit
+
+puts stdout ""
+puts stdout "Funciona somente com o quartus_stp"
+puts stdout ""
+
+set DEV_NAME "@1: 5CE(BA4|FA4) (0x02B050DD)"
+
+set MIF {/home/borg/pfe/RV32I/L2IP/swC/build/firmware.mif}
+
+set JTAG {USB-Blaster [1-4]}
+
+begin_memory_edit -hardware_name $JTAG -device_name $DEV_NAME
+
+update_content_to_memory_from_file -instance_index 0 -mem_file_path $MIF -mem_file_type "mif"
+
+end_memory_edit;
+
+set MEMID {ROM}
